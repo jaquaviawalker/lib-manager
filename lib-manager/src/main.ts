4dffe21f06df903a4c5d1c925b3a8c944f34913c
Jaquavia Walker <jaquavia.walker@copeland.com>
2025-08-19 12:12:24 -0400

    Created member class
    Co-authored-by: Elmehdi Aitbrahim <eaitbrahim@gmail.com>

--- lib-manager/src/main.ts
+++ lib-manager/src/main.ts
@@ -8,17 +8,34 @@ class Book {
 
   private static bookCount: number;
 
-  constructor(
-    title: string,
-    author: string,
-    year: number,
-    isbn: number,
-    bookCount: number
-  ) {
+  constructor(title: string, author: string, year: number, isbn: number) {
     this.title = title;
     this.author = author;
     this.publishedYear = year;
     this.isbn = isbn;
     Book.bookCount++;
+  }
+}
+
+// 2.	Member Class
+// •	Properties: name, memberId
+// •	static totalMembers → track how many members are registered
+// •	Method:
+// •	borrowBook(book: Book) → prints a message like Alice borrowed "The Hobbit"
+
+class Member {
+  name: string;
+  private memberId: number;
+
+  private static totalMembers: number;
+
+  constructor(name: string, memberId: number) {
+    this.name = name;
+    this.memberId = memberId;
+    Member.totalMembers++;
+  }
+
+  borrowBook(book: Book): void {
+    console.log(`${this.name} borrowed ${book.title}`);
   }
 }
